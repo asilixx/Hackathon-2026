5966971372c6caf128f1c7b0d042f073e0eaa566
Paul Lamarque <p.lamarque33290@gmail.com>
2026-04-01 09:48:41 +0200

    bug

--- Frontend/src/classes/zombie.js
+++ Frontend/src/classes/zombie.js
@@ -4,8 +4,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 export class Zombie {
     constructor(scene) {
         this.scene = scene;
-
-        // 1. On crée un groupe qui sera le "vrai" corps du zombie dans l'espace 3D
         this.container = new THREE.Group();
         this.scene.add(this.container);
 
@@ -38,11 +36,7 @@ export class Zombie {
         if (this.model) {
             const pos = new THREE.Vector3();
             this.model.getWorldPosition(pos);
-
             const size = new THREE.Vector3(0.5, 2.8, 1);
-
-            // 3. On centre la hitbox sur le zombie
-            // On ajoute (size.y / 2) à la position Y pour que le bas de la box soit aux pieds
             this.hitbox.setFromCenterAndSize(
                 pos.clone().add(new THREE.Vector3(0, size.y / 2, 0)),
                 size
